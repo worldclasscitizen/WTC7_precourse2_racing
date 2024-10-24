87759e89c08d3d045f8ad3232dcf3672efd8ad38
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 07:25:06 +0900

    feat: Car 클래스 구현
    - 자동차 이름과 위치를 저장하는 필드 추가
    - 자동차의 현재 위치를 반환하는 메서드(getPosition) 추가
    - 자동차의 이름을 반환하는 메서드(getCarName) 추가
    - 자동차가 전진할 때 위치를 증가시키는 메서드(movePosition) 추가

--- src/main/java/racingcar/racing/Car.java
+++ src/main/java/racingcar/racing/Car.java
@@ -1,5 +1,18 @@
 package racingcar.racing;
 
 public class Car {
+    private final String name;
+    private int position = 0;
 
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getCarName() { return name; }
+
+    public int getPosition() { return position; }
+
+    public void movePosition() {
+        position++;
+    }
 }
