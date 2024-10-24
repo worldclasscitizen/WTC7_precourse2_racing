68f5f4384ecbc411b3870b80b725e76f53590a87
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 07:29:55 +0900

    feat: InputView 클래스 구현
    - 사용자로부터 자동차 이름을 입력받는 메서드(readCarNames) 추가
    - 사용자로부터 시도 횟수를 입력받는 메서드(readAttemptTimes) 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,23 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.racing.Car;
+import racingcar.racing.RacingMessage;
+
+import java.util.List;
+
 public class InputView {
+    public static String readCarNames() {
+        System.out.println(RacingMessage.INPUT_CAR_NAME_MESSAGE);
+        String userInput = Console.readLine().trim();
+
+        return userInput;
+    }
+
+    public static String readAttemptTimes() {
+        System.out.println(RacingMessage.INPUT_ATTEMPT_TIMES_MESSAGE);
+        String userInput = Console.readLine().trim();
+
+        return userInput;
+    }
 }
