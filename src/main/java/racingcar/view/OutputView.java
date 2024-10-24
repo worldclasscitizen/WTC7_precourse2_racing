bfe7ca6352725d239238374cdcbc9fd844c8bd7d
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 07:36:29 +0900

    feat: OutputView 클래스 구현
    - 경주 결과 메시지를 출력하는 메서드(printResult) 추가
    - 우승자 명단을 출력하는 메서드(printWinners) 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,21 @@
 package racingcar.view;
 
+import racingcar.racing.Car;
+import racingcar.racing.RacingMessage;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
+    public static void printResult() {
+        System.out.println("\n" + RacingMessage.EXECUTION_RESULT_MESSAGE);
+    }
+
+    public static void printWinners(List<Car> winners) {
+        String winnerNames = winners.stream()
+                .map(Car::getCarName)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(RacingMessage.FINAL_WINNER_MESSAGE + winnerNames);
+    }
 }
