82cb748dfe018c42d9ef70b7ba61ec3c473c251c
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 07:27:38 +0900

    feat: RacingMessage 클래스 구현
    - 자동차 이름 입력 메시지 추가
    - 시도 횟수 입력 메시지 추가
    - 경주 실행 결과 메시지 추가
    - 최종 우승자 메시지 추가

--- src/main/java/racingcar/racing/RacingMessage.java
+++ src/main/java/racingcar/racing/RacingMessage.java
@@ -1,4 +1,8 @@
 package racingcar.racing;
 
 public class RacingMessage {
+    public static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_ATTEMPT_TIMES_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    public static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
+    public static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
 }
