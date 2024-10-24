b14e55120671fe0fcd110de2ebb1e42bcba43959
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 07:32:12 +0900

    feat: Parser 클래스 구현
    - 문자열 입력을 자동차 리스트로 변환하는 메서드(convertInputToCars) 추가
    - 문자열 입력을 정수로 변환하는 메서드(parseToInteger) 추가
    - 입력값이 정수가 아닐 경우 IllegalArgumentException 발생

--- src/main/java/racingcar/util/Parser.java
+++ src/main/java/racingcar/util/Parser.java
@@ -1,4 +1,25 @@
 package racingcar.util;
 
+import racingcar.racing.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parser {
+    public static List<Car> convertInputToCars(String userInput) {
+        String[] names = userInput.split(",");
+        List<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name.trim()));
+        }
+        return cars;
+    }
+
+    public static int parseToInteger(String userInput) {
+        try {
+            return Integer.parseInt(userInput.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수가 아닙니다.");
+        }
+    }
 }
