5d7b6f70f3c614841e9dd02ced4b99a727394432
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 08:24:25 +0900

    feat: RacingController 및 Application 클래스 구현
    - RacingController 클래스에서 게임 초기화, 경주 진행, 우승자 발표 기능 구현
    - Application 클래스에서 메인 메서드를 통해 게임 실행

--- src/main/java/racingcar/racing/RacingController.java
+++ src/main/java/racingcar/racing/RacingController.java
@@ -1,4 +1,47 @@
 package racingcar.racing;
 
+import racingcar.util.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class RacingController {
+    private final Computer computer;
+
+    public RacingController() {
+        this.computer = new Computer();
+    }
+
+    public void start() {
+        initializeGame();
+
+        race();
+
+        announceWinners();
+    }
+
+    private void initializeGame() {
+        List<Car> cars = InputView.readCarNames();
+        int times = InputView.readAttemptTimes();
+
+        InputValidator.validateInputs(cars, times);
+
+        computer.prepareGame(cars, times);
+    }
+
+    private void race() {
+        OutputView.printResult();
+        int totalRounds = computer.getTimes();
+
+        for (int round = 0; round < totalRounds; round++) {
+            computer.moveCars();
+            OutputView.printRaceStatus(computer.getCars());
+        }
+    }
+
+    private void announceWinners() {
+        List<Car> winners = computer.determineWinners();
+        OutputView.printWinners(winners);
+    }
 }
