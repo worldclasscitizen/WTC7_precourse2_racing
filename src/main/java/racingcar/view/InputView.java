248bcae99eeae375634801eb2e0487dcf30c4ab6
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 07:34:53 +0900

    refactor(InputView): 입력값을 로직에 맞는 타입으로 변환하여 반환
    - readCarNames 메서드에서 문자열을 자동차 리스트로 변환하도록 Parser 적용
    - readAttemptTimes 메서드에서 문자열을 정수로 변환하도록 Parser 적용

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,21 +3,22 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.racing.Car;
 import racingcar.racing.RacingMessage;
+import racingcar.util.Parser;
 
 import java.util.List;
 
 public class InputView {
-    public static String readCarNames() {
+    public static List<Car> readCarNames() {
         System.out.println(RacingMessage.INPUT_CAR_NAME_MESSAGE);
         String userInput = Console.readLine().trim();
 
-        return userInput;
+        return Parser.convertInputToCars(userInput);
     }
 
-    public static String readAttemptTimes() {
+    public static int readAttemptTimes() {
         System.out.println(RacingMessage.INPUT_ATTEMPT_TIMES_MESSAGE);
         String userInput = Console.readLine().trim();
 
-        return userInput;
+        return Parser.parseToInteger(userInput);
     }
 }
