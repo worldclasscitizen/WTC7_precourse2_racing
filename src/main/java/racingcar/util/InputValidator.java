92e3372a84c131b004ffc1b3a66a2cab7a154d45
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 07:49:43 +0900

    feat: InputValidator 클래스 구현
    - 자동차 이름의 중복과 길이를 검증하는 validateCarName 메서드 추가
    - 경주 시도 횟수가 양의 정수인지 검증하는 validateAttemptTimes 메서드 추가
    - 중복된 자동차 이름 및 잘못된 이름 길이에 대한 예외 처리

--- src/main/java/racingcar/util/InputValidator.java
+++ src/main/java/racingcar/util/InputValidator.java
@@ -1,4 +1,38 @@
 package racingcar.util;
 
+import racingcar.racing.Car;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputValidator {
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static void validateCarName(List<Car> cars) {
+        Set<String> carNames = new HashSet<>();
+
+        for(Car car : cars) {
+            String carName = car.getCarName();
+
+            if(carNames.contains(carName)) {
+                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_CAR_NAME_ERROR_MESSAGE.getMessage());
+            }
+
+            validateCarNameLength(carName);
+
+            carNames.add(carName);
+        }
+    }
+
+    private static void validateCarNameLength(String carName) {
+        if(carName.isEmpty() || carName.length() > MAX_CAR_NAME_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CAR_NAME_LENGTH_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    public static void validateAttemptTimes(int times) {
+        if(times <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NATURAL_NUMBER_ERROR_MESSAGE.getMessage());
+        }
+    }
 }
