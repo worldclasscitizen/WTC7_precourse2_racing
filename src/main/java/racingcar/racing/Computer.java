d90507ca62c14dd1b943d571025cb789263e29b6
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 08:00:16 +0900

    feat: Computer 클래스와 Validator 클래스 기능 구현
    - Computer 클래스에서 자동차 경주를 준비하는 prepareGame 메서드
    - 랜덤 숫자에 따라 자동차를 이동시키는 moveCars 메서드
    - 최대 위치 값을 찾아 우승자를 결정하는 determineWinners 메서드 추가
    - 랜덤 숫자를 생성하고 이동 여부를 판단하는 shouldMove 메서드
    - Validator 클래스에 자동차 리스트가 비어있지 않은지 검증하는 isNotEmptyCarList 메서드 추가

--- src/main/java/racingcar/racing/Computer.java
+++ src/main/java/racingcar/racing/Computer.java
@@ -1,4 +1,75 @@
 package racingcar.racing;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.util.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+    private final int MIN_RANDOM_NUMBER_RANGE = 0;
+    private final int MAX_RANDOM_NUMBER_RANGE = 9;
+    private List<Car> cars;
+    private int times;
+
+    public Computer() {
+        this.cars = new ArrayList<>();
+    }
+
+    public Computer(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public int getTimes() {
+        return times;
+    }
+
+    public void prepareGame(List<Car> cars, int times) {
+        this.cars = new ArrayList<>();
+        this.times = times;
+    }
+
+    public void moveCars() {
+        cars.forEach(car -> {
+            if(shouldMove()) {
+                car.movePosition();
+            }
+        });
+    }
+
+    public List<Car> determineWinners() {
+        Validator.isNotEmptyCarList(cars);
+
+        int maxPosition = findMaxPosition();
+
+        return selectWinners(maxPosition);
+    }
+
+    private int findMaxPosition() {
+        return cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] position 값을 찾을 수 없습니다."));
+    }
+
+    private List<Car> selectWinners(int maxPosition) {
+        List<Car> winners = new ArrayList<>();
+
+        for(Car car : cars) {
+            if(car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+
+        return winners;
+    }
+
+    private boolean shouldMove() {
+        int randomNumber = pickRandomNumber();
+        return Validator.canMove(randomNumber);
+    }
+
+    private int pickRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER_RANGE, MAX_RANDOM_NUMBER_RANGE);
+    }
 }
--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -10,4 +10,10 @@ public class Validator {
     public static boolean canMove(int randomNumber) {
         return randomNumber >= STANDARD_NUMBER;
     }
+
+    public static void isNotEmptyCarList(List<Car> cars) {
+        if (cars == null || cars.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_LIST_EMPTY_ERROR_MESSAGE.getMessage());
+        }
+    }
 }
