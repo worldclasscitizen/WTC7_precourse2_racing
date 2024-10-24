c21e01483232ef322dc11ffae1fe7e91ba0a2296
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-25 07:52:05 +0900

    feat: Validator 클래스 구현
    - 랜덤 숫자가 기준값(4) 이상일 때 자동차가 움직일 수 있는지 검증하는 canMove 메서드 추가

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -1,4 +1,13 @@
 package racingcar.util;
 
+import racingcar.racing.Car;
+
+import java.util.List;
+
 public class Validator {
+    public static final int STANDARD_NUMBER = 4;
+
+    public static boolean canMove(int randomNumber) {
+        return randomNumber >= STANDARD_NUMBER;
+    }
 }
